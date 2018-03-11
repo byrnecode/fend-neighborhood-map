ed5267598031ad559c888779c96f3eebf9abafd9
Byrne Gabrielle Libanan <byrne.code@gmail.com>
2018-03-11 23:27:51 +0800

    Added comments, map styles, new locations..

--- js/locations.js
+++ js/locations.js
@@ -25,12 +25,12 @@ var locations = [
 	  lng:  123.898203
 	},
 	{
-		title: 'Chatime Ayala',
+		title: 'Chatime Ayala Mall',
 	  lat: 10.318237,
 	  lng:  123.905138
 	},
 	{
-		title: 'Dakasi Ayala',
+		title: 'Dakasi Ayala Mall',
 	  lat: 10.318240,
 	  lng:  123.905147
 	},
@@ -38,5 +38,45 @@ var locations = [
 		title: 'Bubble Tea Station',
 	  lat: 10.340810,
 	  lng:  123.912916
+	},
+	{
+		title: 'Beanleaf',
+	  lat: 10.325358,
+	  lng:  123.919054
+	},
+	{
+		title: 'Bubble Tea Station',
+	  lat: 10.310588,
+	  lng:  123.891870
+	},
+	{
+		title: 'Bubble Tea Station',
+	  lat: 10.309442,
+	  lng:  123.893956
+	},
+	{
+		title: 'Infinitea',
+	  lat: 10.306544,
+	  lng:  123.894708
+	},
+	{
+		title: 'QQ Milk Tea',
+	  lat: 10.300661,
+	  lng:  123.898782
+	},
+	{
+		title: 'Gong Cha',
+	  lat: 10.304142,
+	  lng:  123.911666
+	},
+	{
+		title: 'Bubble Tea Station',
+	  lat: 10.312671,
+	  lng:  123.918247
+	},
+	{
+		title: 'Gong Cha',
+	  lat: 10.312255,
+	  lng:  123.919169
 	}
 ];
--- js/viewModel.js
+++ js/viewModel.js
@@ -2,22 +2,27 @@
 // ==================================
 function viewModel() {
 
-  const model = this;
+  const model = this; // assign 'this' to a variable for inner scope usage
 
-  this.pins = [];
-  this.filterList = ko.observable("");
+  this.pins = []; // array of markers
+  this.filterList = ko.observable(""); // KO observables for filtering list
 
-  this.mapWrapper = document.getElementById('map');
+  this.mapWrapper = document.getElementById('map'); // get map container
+  // map options here fore easier customization
   this.mapOptions = {
-    center: new google.maps.LatLng(10.328620, 123.905306),
-    zoom: 15
+    center: new google.maps.LatLng(10.318237, 123.905138),
+    zoom: 15,
+    styles: mapStyles
   };
 
   this.fsID = 'MEDLG3UM4RPFOX2RSPR1HNHBBSPBXWLOBJLDYHC5ZO4L2Z23'; // Foursquare Client ID
   this.fsSecret = '1WI13SRQ3JSSVIZ30MXPO4Y5EZATDKTJTW1151KYKWI040JL'; // Foursquare Client Secret
 
   // Function to display Info Window
+  // connect to Foursquare API then populate the infoWindow content
+  // @params: marker, infoWindow
   this.showInfoWindow = function(marker, infoWindow) {
+
     if (infoWindow.marker != marker) {
       infoWindow.setContent('');
       infoWindow.marker = marker;
@@ -28,30 +33,30 @@ function viewModel() {
         '&client_secret=' + model.fsSecret + '&query=' + marker.title +
         '&v=20170708' + '&m=foursquare';
 
-      // get Foursquare data
+      // AJAX - Get Foursquare data
       $.getJSON(fsApiUrl)
-      .done(function(marker) {
-        const response = marker.response.venues[0];
+      .done(function (data) { // if success, set infoWindow contents
+        const response = data.response.venues[0];
         model.street = response.location.formattedAddress[0];
         model.city = response.location.formattedAddress[1];
         model.country = response.location.formattedAddress[2];
         model.category = response.categories[0].name;
 
-        model.htmlContentFoursquare = `<h5>(${model.category})</h5>
+        model.fsContent = `<h5>(${model.category})</h5>
                 	<div><h6>Address: </h6>
                 	<p>${model.street}</p>
                 	<p>${model.city}</p>
                 	<p>${model.country}</p></div></div>`;
 
-        infoWindow.setContent(model.htmlContent + model.htmlContentFoursquare);
+        infoWindow.setContent(model.iwContent + model.fsContent);
       })
-      .fail(function() {
+      .fail(function () { // display error msg if failed to connect to Foursquare API
         Materialize.toast('Failed to connect to Foursquare API! Try refreshing your browser.', 60000, 'red darken-1 z-depth-2');
       });
 
-      this.htmlContent = `<div><h4>${marker.title}</h4>`;
+      this.iwContent = `<div><h4>${marker.title}</h4>`; // get marker title for initial infoWindow content
 
-      infoWindow.open(map, marker);
+      infoWindow.open(map, marker); // show infoWindow
 
       infoWindow.addListener('closeclick', function() {
         infoWindow.marker = null;
@@ -59,30 +64,33 @@ function viewModel() {
     }
   };
 
+  // Function to focus on the selected marker/location
+  // and animate the pin
   this.focusMarker = function() {
-
     model.showInfoWindow(this, model.mainInfoWindow);
-    this.setAnimation(google.maps.Animation.BOUNCE);
-
+    this.setAnimation(google.maps.Animation.BOUNCE); // notice me senpai..
+    // stop animation after a set time
     setTimeout((function() {
       this.setAnimation(null);
     }).bind(this), 1400);
-
   };
 
+  // Function to initialize Map
   this.initMap = function() {
 
-    // 
+    // set map constructor
     const map = new google.maps.Map(model.mapWrapper, model.mapOptions);
 
-    //
+    // initial infoWindow
     this.mainInfoWindow = new google.maps.InfoWindow();
 
+    // loop through the locations array
+    // and display markers in map
     for (const value of locations) {
       this.markerTitle = value.title;
       this.markerLat = value.lat;
       this.markerLng = value.lng;
-      // Google Maps marker setup
+      // setup markers
       this.marker = new google.maps.Marker({
         map: map,
         position: {
@@ -95,18 +103,20 @@ function viewModel() {
         animation: google.maps.Animation.DROP
       });
       this.marker.setMap(map);
-      this.pins.push(this.marker);
+      this.pins.push(this.marker); // add locations list to array
       this.marker.addListener('click', model.focusMarker);
     }
 
   };
 
-  this.initMap();
+  this.initMap(); // run initMap function
 
-  // 
+  // Function to add locations array to sidebar
+  // and add filtering feature
   this.locationsFilter = ko.computed(function() {
     let result = [];
 
+    // loop through lcoations list
     for (const value of this.pins) {
       const markerLocation = value;
       if (markerLocation.title.toLowerCase().includes(this.filterList()
